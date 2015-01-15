e19cb1f5f1ba561994994863a250196dbc00de06
Noha Alon <nohalon@gmail.com>
2015-01-14 21:02:13 -0800

    Initial Commit

---
+++ ScientificDrillingMRDD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScientificDrillingMRDD
+//
+//  Created by Noha Alon on 1/14/15.
+//  Copyright (c) 2015 Noha Alon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
