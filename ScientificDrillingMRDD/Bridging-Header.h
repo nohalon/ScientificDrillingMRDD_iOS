de2d91d75d44c430be56152048e21b5d6a251856
Noha Alon <nohalon@gmail.com>
2015-01-15 13:14:09 -0800

    Adding the bridging header file

---
+++ ScientificDrillingMRDD/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  ScientificDrillingMRDD
+//
+//  Created by Noha Alon on 1/14/15.
+//  Copyright (c) 2015 Noha Alon. All rights reserved.
+//
+
+#ifndef ScientificDrillingMRDD_Bridging_Header_h
+#define ScientificDrillingMRDD_Bridging_Header_h
+#import <GooglePlus/GooglePlus.h>
+#import <GoogleOpenSource/GoogleOpenSource.h>
+
+#endif
