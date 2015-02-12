0618ab24d2a508ff4ff54b81b80c69ea7b88ebe6
Noha Alon <nohalon@gmail.com>
2015-02-11 18:50:14 -0800

    side bar

---
+++ ScientificDrillingMRDD/SideBarTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SideBarTableViewController.swift
+//  ScientificDrillingMRDD
+//
+//  Created by Noha Alon on 1/30/15.
+//  Copyright (c) 2015 Noha Alon. All rights reserved.
+//
+
+import UIKit
+
+protocol SideBarTableViewControllerDelegate {
+    func sideBarControlDidSelectRow(indexPath : NSIndexPath)
+}
+
+class SideBarTableViewController: UITableViewController {
+    
+    var delegate : SideBarTableViewControllerDelegate?
+    var tableData : Array<String> = []
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell : UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+            
+            // Configure the cell...
+            cell!.backgroundColor = UIColor.clearColor()
+            //cell!.backgroundColor = UIColor.lightGrayColor()
+            
+            cell!.textLabel!.textColor = UIColor.darkTextColor()
+            
+            let selectedView : UIView = UIView(frame: CGRect(x : 0, y : 0, width : cell!.frame.size.width, height : cell!.frame.size.height))
+            selectedView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.3)
+            
+            cell!.selectedBackgroundView = selectedView
+            
+        }
+        
+        cell!.textLabel!.text = tableData[indexPath.row]
+        
+        
+        return cell!
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 45.0
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        delegate?.sideBarControlDidSelectRow(indexPath)
+    }
+}
