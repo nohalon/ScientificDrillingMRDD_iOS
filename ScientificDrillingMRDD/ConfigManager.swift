7983f5e6e7cbc59602efd52a161414b5b44f82ac
Kevin Backers <kbackers@calpoly.edu>
2015-02-13 16:34:52 -0800

    Config.plist added for settings/metadata and ConfigManager.swift added to access settings

---
+++ ScientificDrillingMRDD/ConfigManager.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigManager.swift
+//  ScientificDrillingMRDD
+//
+//  Created by Kevin Backers on 2/12/15.
+//  Copyright (c) 2015 Noha Alon. All rights reserved.
+//
+
+import Foundation
