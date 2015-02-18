d2d52d66b7b123c408bca54988a84cca842d70e2
Jonathan Pae <jpae@Mac-80e650170f92.zyxel.com>
2015-02-17 18:46:33 -0800

    initial commit for log in

---
+++ ScientificDrillingMRDD/LoginWebViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginWebViewController.swift
+//  ScientificDrillingMRDD
+//
+//  Created by Jonathan Pae on 2/17/15.
+//  Copyright (c) 2015 Noha Alon. All rights reserved.
+//
+
+import Foundation
