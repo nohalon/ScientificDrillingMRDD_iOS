f9d6365389bd42dac47e8d30f3b04bbcb82c89da
Jonathan Pae <jpae@pcp077126pcs.wireless.calpoly.edu>
2015-03-09 13:18:47 -0700

    new backend files

--- ScientificDrillingMRDDBackend/manage.py
+++
@@ -1,2 +0,0 @@
-if __name__ == "__main__":
-    print("STUB: manage.py will eventually deploy new versions of the data service or API, but for now does nothing...")
--- ScientificDrillingMRDDBackend/test_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PYTHON=python2.7
-
-$PYTHON -m unittest discover -p '*_test.py'
